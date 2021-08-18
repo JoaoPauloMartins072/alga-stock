d8752793aa501e16e0f735e627a7978c8d16a650
JoaoPauloMartins072 <joaopaulo_072@outlook.com>
2021-08-18 14:42:11 -0300

    02-10

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,23 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-import TestComponent from '../TestComponent'
+import Header from '../Header';
+import Button from '../../shared/Button';
+import Container from '../../shared/Container';
+import Input from '../../shared/Input';
+
+function TestComponent () {
+  return <img width="16" src="https://img.icons8.com/pastel-glyph/2x/search--v2.png" alt="search icon" />
+}
 
 function App() {
+  const [street, setStreet] = useState('')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <TestComponent />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title="AlgaStock" />
+
+      <Container>
+        <ul>
+          {
+            ['Daniel', 'Wiliiam', 'Thiago', 'Daniel'].map ((name, index) => {
+              return <li key={index}>
+
+              { name }
+              </li>
+            })
+          }
+
+        </ul>
+       
+          
+      
+      </Container>
     </div>
   );
 }
