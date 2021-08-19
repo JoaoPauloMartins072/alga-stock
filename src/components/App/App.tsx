e00b9e9e0653a025b7db115b291bf2ded3edfea4
JoaoPauloMartins072 <joaopaulo_072@outlook.com>
2021-08-19 08:45:02 -0300

    estilização

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,34 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import Header from '../Header';
-import Button from '../../shared/Button';
 import Container from '../../shared/Container';
-import Input from '../../shared/Input';
+import Table from '../../shared/Table';
 
 function TestComponent () {
   return <img width="16" src="https://img.icons8.com/pastel-glyph/2x/search--v2.png" alt="search icon" />
 }
 
 function App() {
-  const [street, setStreet] = useState('')
-
   return (
     <div className="App">
       <Header title="AlgaStock" />
 
       <Container>
-        <ul>
-          {
-            ['Daniel', 'Wiliiam', 'Thiago', 'Daniel'].map ((name, index) => {
-              return <li key={index}>
 
-              { name }
-              </li>
-            })
-          }
-
-        </ul>
-       
+      <Table />
           
       
       </Container>
