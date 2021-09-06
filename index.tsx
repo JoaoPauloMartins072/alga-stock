32d407ad359e8dbe375f669a486bee0dc2e40e16
JoaoPauloMartins072 <joaopaulo_072@outlook.com>
2021-09-06 17:05:17 -0300

    4.2

--- index.tsx
+++ index.tsx
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './src/components/App/App';
 import http from './src/utils/http'
+import { Provider } from 'react-redux'
+import store from './redux'
+
 
 http.get('./post')
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
