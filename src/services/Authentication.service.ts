1d05e54634da7f5d729d7ff9ab356905798d983e
JoaoPauloMartins072 <joaopaulo_072@outlook.com>
2021-09-20 16:48:24 -0300

    5.7

---
+++ src/services/Authentication.service.ts
@@ -0,0 +1,16 @@
+import http from "../utils/http";
+
+export declare interface User {
+    _id: string
+    user: string
+    email: string
+    role: 'admin' | 'customer'
+    token: string
+    createdAt: string
+    updatedAt: string
+}
+
+export const signInUser = (user: string, pass: string ) =>
+    http    
+     .post('/authentication/login', { user, pass})
+     .then(res => res.data)
